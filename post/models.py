5b8c9361bf5cf9852e559f25adeaf9dea765f44e
ahmedazab1235 <ahmedazab1235@yahoo.com>
2020-12-31 20:18:44 +0200

    create first Post model in post app

--- post/models.py
+++ post/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+from django.utils import timezone
+
 # Create your models here.
+
+
+
+class Post(models.Model):
+    auther = models.CharField(max_length=50)
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    created_date = models.DateTimeField(default = timezone.now)
+    content = models.TextField(max_length=200000)
+    #love = models.ManyToManyField()
+    #unicorn = models.ManyToManyField()
+
+
+    def __str__(self):
+        return self.title
+
+    
+
